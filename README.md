52e611f57d30746192cfd2fc5e98d0dbde048be0
Chris Zavala <frappejcat@gmail.com>
2023-01-22 01:13:57 -0600

    adding the rest of the generateMarkdown.js file still not working

--- README.md
+++ README.md
@@ -34,9 +34,11 @@ Provide instructions and examples for use. Include screenshots as needed.
 
 To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
 
-    ```md
-    ![alt text](assets/images/screenshot.png)
-    ```
+The application will be invoked by using the following command:
+
+```bash
+node index.js
+```
 
 ## Credits
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,26 +1,70 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let badgeLabel = license.replace("", "&ensp;");
-  return `[![Generic badge](https://img.shields.io/badge/License-${badgeLabel}-silver.svg)](${renderLicenseLink(license)})`
+  let badgeLabel = license.replace(" ", "&ensp;");
+  return `
+  [![Generic badge](https://img.shields.io/badge/License-${badgeLabel}-green.svg)](${renderLicenseLink(license)})
+  `
 };
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  let URL = license.toLowerCase().replace("", "-");
-  return `https://choosealicense.com/licenses/${URL}/.`
-}
+  let linkUrl = license.toLowerCase().replace(" ", "-");
+  return `https://choosealicense.com/licenses/${linkUrl}/.`
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  
-}
+  if (license === "None") {
+    return "";
+
+  }else{
+    return `
+    ### Licensing 
+    ${license}
+    ${renderLicenseBadge(license)}`
+  }
+
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let {license, GitHub } = data
+  return `
+  # ${data.title}
+
+  ##Description 
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ##Installation 
+  ${data.installation}
+
+  ##Usage
+  ${data.provideInstruction}
+
+  ##Credits
+  ${data.credit}
+  List of GitHub contributors, [${GitHub}](https://gitbut.com/${GitHub})
+
+  ##License
+  ${renderLicenseSection(license)}
+
+  ##Features
+  ${data.features}
+
+  ##How to Contribute
+  ${data.contributions}
+
+  ##Test
+  ${data.tests}
 
 `;
 }
